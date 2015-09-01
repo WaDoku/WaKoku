9bd27c3a80685cf4a6eecd1f6073523a13c36aa0
Jiayi Zheng <thebluber@gmail.com>
2015-09-01 16:05:46 +0200

    Add validations for image in entry model + tests

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,4 +2,11 @@ class Entry < ActiveRecord::Base
   dragonfly_accessor :image
   validates_presence_of :writing
   validates_presence_of :kana
+
+  validates_size_of :image, maximum: 500.kilobytes,
+    message: "Datei überschreitet das Uploadlimit", if: :image_changed?
+
+  validates_property :mime_type, of: :image, in: ["image/jpg", "image/svg+xml", "image/png", "image/jpeg"],
+    message: "Format nicht zugelassen", if: :image_changed?
+
 end
--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -3,4 +3,26 @@ require 'rails_helper'
 RSpec.describe Entry, type: :model do
   it { should validate_presence_of(:writing) }
   it { should validate_presence_of(:kana) }
+
+  describe "image" do
+    let(:entry){ create :entry }
+
+    it "should validate image size" do
+      entry.image = File.open("spec/fixtures/haniwa.svg") #< 500kb
+      expect(entry.save).to be true
+      entry.image = File.open("spec/fixtures/komiketo.jpg") #> 500kb
+      expect(entry.save).to be false
+      expect(entry.errors.full_messages).to eq ["Image Datei überschreitet das Uploadlimit"]
+    end
+
+    it "should validate the mime_type of image" do
+      entry.image = File.open("spec/fixtures/haniwa.svg")
+      expect(entry.save).to be true
+      entry.image = File.open("spec/fixtures/waterfall.png")
+      expect(entry.save).to be true
+      entry.image = File.open("spec/fixtures/PflichtenheftWaKoku.pdf")
+      expect(entry.save).to be false
+      expect(entry.errors.full_messages).to eq ["Image Format nicht zugelassen"]
+    end
+  end
 end
