bf69cacd701591c9e531ea0f1b7e0be1b9d71d8d
Jiayi Zheng <thebluber@gmail.com>
2015-09-04 15:54:41 +0200

    Redirect /admin to /admin/entries for correct routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
 
 
   namespace :admin do
-    get "/" => "entries#index"
+    get "/", to: redirect("admin/entries")
     get "log_out" => "sessions#destroy",  as: :log_out
     get "log_in" => "sessions#new", as: :log_in
     resources :sessions
